89b797c59b570d5507299a5d4e9494d8de7036df
blockchaindev0125 <blockchaindev0125@gmail.com>
2022-06-26 21:16:47 -0500

    init

---
+++ stencil.config.ts
@@ -0,0 +1,56 @@
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
+import { postcss } from "@stencil/postcss";
+import autoprefixer from "autoprefixer";
+import nodePolyfills from "rollup-plugin-node-polyfills";
+
+export const config: Config = {
+  namespace: "stellar-wallet",
+  devServer: {
+    openBrowser: false,
+  },
+  outputTargets: [
+    {
+      type: "dist",
+      esmLoaderPath: "../loader",
+    },
+    {
+      type: "docs-readme",
+    },
+    {
+      type: "www",
+      serviceWorker: null, // disable service workers
+      baseUrl: "http://localhost:3333/",
+    },
+  ],
+  globalStyle: "src/global/style.scss",
+  commonjs: {
+    namedExports: {
+      "stellar-sdk": [
+        "StrKey",
+        "xdr",
+        "Transaction",
+        "Keypair",
+        "Networks",
+        "Account",
+        "TransactionBuilder",
+        "BASE_FEE",
+        "Operation",
+        "Asset",
+        "Memo",
+        "MemoHash",
+      ],
+    },
+  },
+  plugins: [
+    nodePolyfills(),
+    sass(),
+    postcss({
+      plugins: [autoprefixer()],
+    }),
+  ],
+  nodeResolve: {
+    browser: true,
+    preferBuiltins: true,
+  },
+};
